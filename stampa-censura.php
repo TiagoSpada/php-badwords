9998a063ac27baa9742ca064d4c73089366f443b
TiagoSpada <tiago.spada.ts@gmail.com>
2024-06-17 18:03:55 +0200

    [UPD] sostituzione della parola e stampa dei paragrafi

--- stampa-censura.php
+++ stampa-censura.php
@@ -2,3 +2,21 @@
 
 $paragrafo = $_GET['paragrafo'];
 $parola_da_censurare = $_GET['censura'];
+trim($parola_da_censurare);
+$paragrafo_censurato = str_replace($parola_da_censurare, '***', $paragrafo);
+?>
+    
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Stampa paragrafo</title>
+</head>
+<body>
+    <h2>Paragrafo (lunghezza):</h2>
+    <p> <?php echo $paragrafo . ' (' . strlen($paragrafo) .')' ?></p> 
+    <h2>Paragrafo Censurato (lunghezza):</h2>
+    <p> <?php echo $paragrafo_censurato . ' (' . strlen($paragrafo) .')' ?></p> 
+</body>
+</html>
